71821f7d8a698d9282b6a2fafd1778fc90a7736c
Pavel_Orekhov <Pavel_Orekhov@epam.com>
2019-04-03 16:53:58 +0300

    Wrote comments.

--- s3_working_with_directories/src/main/scala/pack/Main.scala
+++ s3_working_with_directories/src/main/scala/pack/Main.scala
@@ -22,9 +22,11 @@ object Main {
   val secretKey = "***"
   val bucketName = "***"
 
-  def main(args: Array[String]): Unit = {
-
-    // Create spark
+  /**
+   * Creates a spark session with all the required settings.
+   * @return spark session.
+   */
+  def createSpark(): SparkSession = {
     val spark = SparkSession.builder()
       .master("local[*]")
       .appName("App")
@@ -36,40 +38,67 @@ object Main {
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
     System.setProperty(SDKGlobalConfiguration.ENABLE_S3_SIGV4_SYSTEM_PROPERTY, "true")
 
+    spark
+  }
 
-    // Create amazon client
+  /**
+   * Creates amazon s3 client with all the required settings.
+   * @return an s3 client instance.
+   */
+  def createAmazonS3Client(): AmazonS3Client = {
     val credentials = new BasicAWSCredentials(accessKey, secretKey)
     val s3client = new AmazonS3Client(credentials, new ClientConfiguration().withProtocol(Protocol.HTTP))
     s3client.setEndpoint("s3.us-east-2.amazonaws.com")
 
-//    if (!doesPathExist(s3client, bucketName, "output")) {
-//      createFolder(s3client, bucketName, "output/")
-//    }
-//
-//    //renamePathsWithPrefix(s3client, bucketName, "testFolder", "tmp")
-//
-//    val df = spark.read
-//      .option("inferSchema", "true")
-//      .option("header", "true")
-//      .csv("s3a://pavel.orekhov.testing.bucket/taxonomy.csv")
-//      .withColumn("date_load", lit(getCurrentDate))
-//
-//    df.repartition(1)
-//      .write
-//      .partitionBy("date_load")
-//      .mode("overwrite")
-//      .option("header", "true")
-//      .csv("s3a://pavel.orekhov.testing.bucket/output/")
-//
-//    println(df.count())
+    s3client
   }
 
-//  def getCurrentDate: String = {
-//    val dtf = DateTimeFormatter.ofPattern("yyyyMMdd")
-//    LocalDateTime.now.format(dtf)
-//  }
+  def main(args: Array[String]): Unit = {
 
-  def createFolder(s3client: AmazonS3, bucketName: String, folderName: String): Unit = { // create meta-data for your folder and set content-length to 0
+    val spark = createSpark()
+    val s3Client = createAmazonS3Client()
+
+    // TODO: uncomment if you want to test
+
+    //    if (!doesPathExist(s3client, bucketName, "output")) {
+    //      createFolder(s3client, bucketName, "output/")
+    //    }
+    //
+    //    //renamePathsWithPrefix(s3client, bucketName, "testFolder", "tmp")
+    //
+    //    val df = spark.read
+    //      .option("inferSchema", "true")
+    //      .option("header", "true")
+    //      .csv("s3a://pavel.orekhov.testing.bucket/taxonomy.csv")
+    //      .withColumn("date_load", lit(getCurrentDate))
+    //
+    //    df.repartition(1)
+    //      .write
+    //      .partitionBy("date_load")
+    //      .mode("overwrite")
+    //      .option("header", "true")
+    //      .csv("s3a://pavel.orekhov.testing.bucket/output/")
+    //
+    //    println(df.count())
+  }
+
+  // TODO: uncomment if you want to test
+
+  //  def getCurrentDate: String = {
+  //    val dtf = DateTimeFormatter.ofPattern("yyyyMMdd")
+  //    LocalDateTime.now.format(dtf)
+  //  }
+
+  /**
+   * Create folder.
+   *
+   * Important: folderName must end with "/" to create a folder instead of a file.
+   *
+   * @param s3client   s3 client.
+   * @param bucketName bucket name.
+   * @param folderName name of folder.
+   */
+  def createFolder(s3client: AmazonS3, bucketName: String, folderName: String): Unit = {
     val metadata = new ObjectMetadata
     metadata.setContentLength(0)
     val emptyContent = new ByteArrayInputStream(new Array[Byte](0))
@@ -77,6 +106,13 @@ object Main {
     s3client.putObject(putObjectRequest)
   }
 
+  /**
+   * Emulates folder deletion, if prefix is name to folder (sort of like full path).
+   *
+   * @param s3client   s3 client.
+   * @param bucketName bucket name.
+   * @param prefix     prefix, such as "path/to/folder".
+   */
   def deletePathsWithPrefix(s3client: AmazonS3, bucketName: String, prefix: String): Unit = {
     val listObjectsRequest = new ListObjectsRequest()
       .withBucketName(bucketName)
@@ -97,11 +133,27 @@ object Main {
     }
   }
 
+  /**
+   * Emulates check for folder existence.
+   *
+   * @param s3client   s3 client.
+   * @param bucketName bucket name.
+   * @param prefix     prefix, such as "path/to/folder".
+   * @return is there at least one file with this prefix? True or false?
+   */
   def doesPathExist(s3client: AmazonS3, bucketName: String, prefix: String): Boolean = {
     s3client.listObjects(bucketName, prefix)
       .getObjectSummaries.size() > 0
   }
 
+  /**
+   * Renames all files that have the specified prefix.
+   *
+   * @param s3client   s3 client.
+   * @param bucketName bucket name.
+   * @param oldPrefix  prefix to rename.
+   * @param newPrefix  new prefix.
+   */
   def renamePathsWithPrefix(s3client: AmazonS3, bucketName: String, oldPrefix: String, newPrefix: String): Unit = {
     val listObjectsRequest = new ListObjectsRequest()
       .withBucketName(bucketName)
